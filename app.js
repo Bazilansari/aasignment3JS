a003faad4e694bccfba3f9bda3bce292ce035b07
Bazilansari <154603848+Bazilansari@users.noreply.github.com>
2024-03-10 12:56:21 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,204 @@
+// ======MATH EXPRESSIONS=====
+
+1. 
+var numb1 = 3
+var numb2 = 5
+
+var Plus = numb1 + numb2
+
+console.log('Sum of', numb1 ,'and', numb2 , 'is' , Plus);
+
+2.
+var numb1 = 3
+var numb2 = 5
+
+var Sub = numb1 - numb2
+
+console.log('Sub of', numb1 ,'and', numb2 , 'is' , Sub);
+
+
+var numb1 = 3
+var numb2 = 5
+
+var Multiply = numb1 * numb2
+
+console.log('Multiply of', numb1 ,'and', numb2 , 'is' , Multiply);
+
+
+var numb1 = 3
+var numb2 = 5
+
+var Divid = numb1 / numb2
+
+console.log('Divid of', numb1 ,'and', numb2 , 'is' , Divid);
+
+
+var numb1 = 3
+var numb2 = 5
+
+var Reminder = numb1 % numb2
+
+console.log('Reminder of', numb1 ,'and', numb2 , 'is' , Reminder);
+
+3.
+
+var A = 5
+var B =  A++
+
+console.log(A);
+
+
+var A = 5
+var B = 7
+var a =  A++
+var Addition = A + B
+
+console.log(Addition);
+
+var A = 5
+var B = 7
+var a =  A--
+var Addition = a + B
+
+console.log(Addition);
+
+var A = 5
+var B = 7
+var a =  A--
+var Addition = a + B
+var num4 = 3
+var divid = Addition % num4
+
+console.log(divid);
+
+
+4.
+
+var ticket = 600
+var buy = 5
+var multiply = ticket * buy
+
+console.log(multiply);
+
+
+5.
+
+var A = 4
+var b = 1
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 2
+console.log(A+'x'+b+ '=' +A * b );
+
+var b = 3
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 4
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 5
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 6
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 7
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 8
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 9
+console.log(A+'x'+b+ '=' +A * b);
+
+var b = 10
+console.log(A+'x'+b+ '=' +A * b);
+
+
+7.
+
+var item1 = 650
+var item2 = 100
+var item1qty = 3
+var item2qty = 7
+var shipping = 100
+var total = (item1 * item1qty) + (item2 * item2qty) + shipping
+
+console.log(total);
+
+
+8.
+
+var total = 980
+var obtained = 804
+var divide = total / obtained 
+console.log( divide * 100 +'%');
+
+
+9.
+
+var dollar = +prompt('Enter Dollars')
+var Riyal = +prompt('Enter Riyal')
+
+var dollartopkr = 278
+var riyaltopkr = 74
+
+console.log('Your Pkr Rates', dollar * dollartopkr + Riyal * riyaltopkr);
+
+
+10.
+
+var a = 5
+var b = 10
+
+console.log((5 * 10) / 2);
+
+
+11.
+
+var current_year = +prompt('Enter Current Year')
+var birth_year = +prompt('Enter Birth year')
+
+console.log('Your Age is', current_year - birth_year);
+
+
+13.
+
+var Snack = "Lays"
+var Age = 22
+var max_age = 55
+var amount = 2
+var total = max_age - Age
+
+console.log(total * amount);
+
+
+// =====Variable name legal & illegle=======
+
+A = 'bazil'
+B = 'Ansari'
+age =  22
+
+console.log(A,B,age);
+
+// ====illegal Variable=====
+
+A = '2nd_attempt (Starting with a number)'
+B = 'user age (Containing a space)'
+C = 'total-sum (Containing a hyphen)'
+D = '@username (Containing special characters other than underscore)'
+E = 'class (Using reserved keywords, like a programming language keyword)'
+
+
+
+// ====Legal Variable=====
+
+heading = 'Rules for naming JS variables'
+
+A = 'Variable names can only contain, number, $,and_ '
+B = 'variable must being with a letter, $ , or _. '
+c = 'Variable names are case (sensitive)'
+D = 'Variable names should not be JS (Keywords)'
+
+
+
